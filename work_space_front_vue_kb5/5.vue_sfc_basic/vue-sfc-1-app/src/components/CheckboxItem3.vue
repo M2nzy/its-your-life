3a0b62c00441f5f66e18e0bb9dd313adc55ed1a8
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-05-28 16:33:14 +0900

    240528: vue.js

---
+++ work_space_front_vue_kb5/5.vue_sfc_basic/vue-sfc-1-app/src/components/CheckboxItem3.vue
@@ -0,0 +1,15 @@
+<template>
+    <li>
+        <input type="checkbox" :checked="idol.checked"> {{ idol.id }} - {{ idol.name }}
+    </li>
+</template>
+
+<script>
+import idol from '../Idol.js';
+export default{
+    name:'CheckboxItem3',
+    props:{
+        idol:idol,
+    }
+}
+</script>
---
+++ work_space_front_vue_kb5/5.vue_sfc_basic/vue-sfc-1-app/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './assets/main.css'
+import App from './App.vue'
+import App1 from './App1.vue'
+import App2 from './App2.vue'
+import App3 from './App3.vue'
+import App4 from './App4.vue'
+
+createApp(App4).mount('#app')
